3199db40c3d8c7515e39e7efc22c9b97ced61404
priyanshu <jainpriyanshu341@gmail.com>
2020-07-12 00:00:48 +0530

    Initial commit

---
+++ nodeserver/index.js
@@ -0,0 +1,21 @@
+// node server which will handel socket io connection
+const io = require('socket.io')(80
+	)
+const user =  {};
+
+io.on('connection', socket => {
+     socket.on('new-user-joined',name =>{
+		 //console.log("new-user",name)
+		  user[socket.id] = name;
+		  socket.broadcast.emit('user-joined',name);
+	 });
+	 socket.on('send',message =>{
+		socket.broadcast.emit('receive', { message: message , name: user[socket.id] })
+	 });
+
+	 socket.on('disconnect',message =>{
+		socket.broadcast.emit('left', user[socket.id])
+		delete user[socket.id];
+	 });
+
+	})
